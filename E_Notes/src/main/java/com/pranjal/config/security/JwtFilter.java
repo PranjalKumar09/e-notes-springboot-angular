335bb3fbae209c9a70042069a11890efb43d0103
PranjalKumar09 <coderkumarshukla@gmail.com>
2024-10-05 19:32:17 +0530

    🚀 Added JwtFilter for token validation and security filtering

---
+++ E_Notes/src/main/java/com/pranjal/config/security/JwtFilter.java
@@ -0,0 +1,71 @@
+package com.pranjal.config.security;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pranjal.handler.GenericResponse;
+import com.pranjal.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        try {
+            String username = null;
+            String token = null;
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                username =  jwtService.extractUsername(token);
+            }
+            if (username!=null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if (jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                }
+            }
+        }
+        catch (Exception e) {
+           generateResponseError(response, e);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void generateResponseError(HttpServletResponse response, Exception e) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        Object body =   GenericResponse.builder()
+                .status("failed")
+                .message(e.getMessage())
+                .responseStatus(HttpStatus.UNAUTHORIZED)
+                .build().create().getBody();
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+
+    }
+}
---
+++ E_Notes/src/main/java/com/pranjal/service/impl/JwtServiceImpl.java
@@ -0,0 +1,4 @@
+package com.pranjal.service.impl;
+
+public class JwtServiceImpl {
+}
