7c748aa6fe324910502133f40ed15dbd5aad0c78
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-01-19 14:20:14 +0530

    Create CategoryDto to handle data transfer for category operations.

--- E_Notes/src/main/java/com/pranjal/dto/CategoryDto.java
+++ E_Notes/src/main/java/com/pranjal/dto/CategoryDto.java
@@ -1,5 +1,20 @@
 package com.pranjal.dto;
 
-@
-public class Category {
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class CategoryDto {
+    private String name;
+    private String description;
+    private Boolean isActive;
+    private Integer createdBy;
+    private Date createdOn;
+    private Integer updateBy;
+    private Date updateOn;
 }
