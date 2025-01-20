55314bae7451be619da32f7082f32c0a4abb31bb
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-01-20 14:20:14 +0530

    Introduce CategoryReponse to structure API responses for category services.

--- E_Notes/src/main/java/com/pranjal/dto/CategoryReponse.java
+++ E_Notes/src/main/java/com/pranjal/dto/CategoryReponse.java
@@ -1,4 +1,18 @@
 package com.pranjal.dto;
 
-public class CateogryReponse {
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CategoryReponse {
+    private Integer id;
+    private String name;
+    private String description;
 }
