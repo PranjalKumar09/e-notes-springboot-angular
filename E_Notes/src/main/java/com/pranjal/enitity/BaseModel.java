9293aceda55b2c4b09fcf88d647b66cd44fcb98b
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-01-21 14:20:14 +0530

    Define BaseModel to provide a common structure for all entities in E_Notes.

--- E_Notes/src/main/java/com/pranjal/enitity/BaseModel.java
+++ E_Notes/src/main/java/com/pranjal/enitity/BaseModel.java
@@ -1,4 +1,19 @@
 package com.pranjal.enitity;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
 public class BaseModel {
+    private Boolean isActive;
+    private Boolean isDeleted;
+    private Integer createdBy;
+    private Date createdOn;
+    private Integer updateBy;
+    private Date updateOn;
 }
