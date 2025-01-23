07a81b905aeeeadc4454edc46878890e5ba1638d
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-01-23 14:20:14 +0530

    Create CategoryRepository interface for database operations on categories.

--- E_Notes/src/main/java/com/pranjal/repository/CategoryRepository.java
+++ E_Notes/src/main/java/com/pranjal/repository/CategoryRepository.java
@@ -1,4 +1,12 @@
 package com.pranjal.repository;
 
-public class CategoryRepository {
+import com.pranjal.enitity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByIsActiveTrue();
 }
