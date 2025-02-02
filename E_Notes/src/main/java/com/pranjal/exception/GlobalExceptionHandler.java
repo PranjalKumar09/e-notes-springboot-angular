6195d48093f56f3d15f7e84d83d64498f21c2e44
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-02 17:37:01 +0530

    Refactor GlobalExceptionHandler: Added custom error responses for better API error tracking.

--- E_Notes/src/main/java/com/pranjal/exception/GlobalExceptionHandler.java
+++ E_Notes/src/main/java/com/pranjal/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ package com.pranjal.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -18,12 +19,7 @@ import java.util.Map;
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    /*@ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleException(Exception e) {
-        log.error("GlobalExceptionHandler ::  handleException ::", e.getMessage());
 
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }*/
 
 
     @ExceptionHandler(NullPointerException.class)
@@ -32,20 +28,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-/* @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
-
-        Map<String, Object> errors = new LinkedHashMap<>();
-        allErrors.forEach(error -> {
-            String msg = error.getDefaultMessage();
-            String field = ((FieldError)(error)).getField();
-            errors.put(field, msg);
-        });
-
-        log.error("GlobalExceptionHandler ::  MethodArgumentNotValidException ::", e);
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }*/
 
 
     @ExceptionHandler(ResourceNotFoundException.class)
@@ -59,7 +41,21 @@ public class GlobalExceptionHandler {
         log.error("ResourceNotFoundException ::  ValidationException");
         return new ResponseEntity<>(e.getError(), HttpStatus.NOT_FOUND);
     }
+    @ExceptionHandler(ExistDataException.class)
+    public ResponseEntity<?> handleExistDataException(ExistDataException e){
+        log.error("ResourceNotFoundException ::  ExistDataException");
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT );
+    }
 
-    
+
+
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e){
+        log.error("ResourceNotFoundException ::  HttpMessageNotReadableException");
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST );
+    }
+
+
 }
 
