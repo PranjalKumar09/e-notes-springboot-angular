c9e2acb2161146b7f90ce1f69df1ce9ad202a382
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-04 19:09:01 +0530

    Update GlobalExceptionHandler to properly handle custom validation exceptions

--- E_Notes/src/main/java/com/pranjal/exception/GlobalExceptionHandler.java
+++ E_Notes/src/main/java/com/pranjal/exception/GlobalExceptionHandler.java
@@ -4,18 +4,26 @@ package com.pranjal.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
+    /*@ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception e) {
         log.error("GlobalExceptionHandler ::  handleException ::", e.getMessage());
 
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+    }*/
 
 
     @ExceptionHandler(NullPointerException.class)
@@ -24,11 +32,32 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+/* @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+
+        Map<String, Object> errors = new LinkedHashMap<>();
+        allErrors.forEach(error -> {
+            String msg = error.getDefaultMessage();
+            String field = ((FieldError)(error)).getField();
+            errors.put(field, msg);
+        });
+
+        log.error("GlobalExceptionHandler ::  MethodArgumentNotValidException ::", e);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }*/
+
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(Exception e){
         log.error("ResourceNotFoundException ::  handleResourceNotFoundException ::", e.getMessage());
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> handleValidationException(ValidationException e){
+        log.error("ResourceNotFoundException ::  ValidationException");
+        return new ResponseEntity<>(e.getError(), HttpStatus.NOT_FOUND);
     }
 
     
