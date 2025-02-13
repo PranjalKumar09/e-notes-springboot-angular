bd27d8c03c61ee169b48fe8648940ba8ff58d25f
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-13 21:34:30 +0530

    🔧 Introduced NotesService.java: Service interface for note operations

--- E_Notes/src/main/java/com/pranjal/service/NotesService.java
+++ E_Notes/src/main/java/com/pranjal/service/NotesService.java
@@ -1,4 +1,16 @@
 package com.pranjal.service;
 
-public class NotesService {
+import com.pranjal.dto.NotesDto;
+import com.pranjal.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public interface NotesService {
+    Boolean saveNotes(NotesDto notesDto) throws  Exception;
+    List<NotesDto> getAllNotes();
+
+
 }
