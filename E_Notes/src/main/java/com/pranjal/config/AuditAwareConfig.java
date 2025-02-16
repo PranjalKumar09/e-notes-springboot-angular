24d31ba404df2d4a1fd0c73b0e1db9559fbe12c5
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-16 21:34:30 +0530

    🔍 Updated AuditAwareConfig.java: Enhanced audit logging

--- E_Notes/src/main/java/com/pranjal/config/AuditAwareConfig.java
+++ E_Notes/src/main/java/com/pranjal/config/AuditAwareConfig.java
@@ -8,6 +8,6 @@ public class AuditAwareConfig implements AuditorAware<Integer> {
 
     @Override
     public Optional<Integer> getCurrentAuditor() {
-        return Optional.of(1);
+        return Optional.of(2);
     }
 }
