93554da8e044c0e09ac63d00d96b6134d3f451ce
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-19 21:34:30 +0530

    📌 Modified CategoryDto.java: Added new fields for category data

--- E_Notes/src/main/java/com/pranjal/dto/CategoryDto.java
+++ E_Notes/src/main/java/com/pranjal/dto/CategoryDto.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
