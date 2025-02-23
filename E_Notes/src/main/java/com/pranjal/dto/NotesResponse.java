ad87d52c5ec4b9b3f13923770deb46bceef16d50
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-23 21:00:32 +0530

    Added NotesResponse DTO to structure API responses for notes

---
+++ E_Notes/src/main/java/com/pranjal/dto/NotesResponse.java
@@ -0,0 +1,24 @@
+package com.pranjal.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class NotesResponse {
+    private List<NotesDto> notes;
+
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private Boolean isFirst;
+    private Boolean isLast;
+
+
+}
