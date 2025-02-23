472744bb5034123bd53d156962371269a0fe0f1f
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-23 12:28:51 +0530

    📨 Added UserResponse DTO — Streamlined user data responses with cleaner API output.

--- E_Notes/src/main/java/com/pranjal/dto/UserResponse.java
+++ E_Notes/src/main/java/com/pranjal/dto/UserResponse.java
@@ -1,4 +1,44 @@
 package com.pranjal.dto;
+import lombok.*;
 
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class UserResponse {
+
+    private Integer id;
+    private String firstName;
+    private String LastName;
+    private String email;
+    private String password;
+    private String mobno;
+
+    private StatusDto status;
+
+    private List<RoleDto> roles;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    @Builder
+    public static class RoleDto {
+        private Integer id;
+        private String name;
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    @Builder
+    public static class StatusDto {
+        private Integer id;
+        private Boolean isActive;
+    }
 }
