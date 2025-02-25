70052cdf3b0ca52f32414c1eadbfc55bad29d344
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-25 21:00:32 +0530

    Modified NotesRepository to add pagination functionality

--- E_Notes/src/main/java/com/pranjal/repository/NotesRepository.java
+++ E_Notes/src/main/java/com/pranjal/repository/NotesRepository.java
@@ -1,13 +1,18 @@
 package com.pranjal.repository;
 
+import com.pranjal.dto.NotesDto;
 import com.pranjal.enitity.Notes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NotesRepository extends JpaRepository<Notes, Integer> {
 
 
 
-
+    Page<Notes> findByCreatedBy(Integer userId, Pageable pageable);
 }
