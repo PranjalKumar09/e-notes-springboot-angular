40d09a3e7a098b20cd7e41cc5968933da84cd3ee
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-02-26 12:28:52 +0530

    🚀 Enhanced UserService — Improved user creation and profile retrieval with better error handling.

--- E_Notes/src/main/java/com/pranjal/service/UserService.java
+++ E_Notes/src/main/java/com/pranjal/service/UserService.java
@@ -1,4 +1,17 @@
 package com.pranjal.service;
 
-public class UserSERvice {
+import com.pranjal.dto.PasswordChangeRequest;
+import com.pranjal.dto.PswdResetRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    void  changePassword(PasswordChangeRequest passwordChangeRequest);
+
+    void sendEmailPasswordReset(String email, HttpServletRequest httpServletRequest)  throws  Exception;
+
+    void verifyPaswdResetLink(Integer uid, String code) throws Exception;
+
+    void resetPassword(PswdResetRequest pswdResetRequest) throws Exception;
 }
