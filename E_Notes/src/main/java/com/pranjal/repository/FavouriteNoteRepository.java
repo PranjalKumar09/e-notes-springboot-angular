26b348bb4858599e80a547587581b48dbb044782
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-01 19:58:24 +0530

    💾 Added FavouriteNoteRepository: Repository interface for database operations on favorite notes.

--- E_Notes/src/main/java/com/pranjal/repository/FavouriteNoteRepository.java
+++ E_Notes/src/main/java/com/pranjal/repository/FavouriteNoteRepository.java
@@ -1,24 +1,14 @@
 package com.pranjal.repository;
 
-import com.pranjal.enitity.Notes;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.pranjal.dto.FavouriteNoteDto;
+import com.pranjal.enitity.FavouriteNote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface FavouritesNotesRepository extends JpaRepository<Notes, Integer> {
+public interface FavouriteNoteRepository extends JpaRepository<FavouriteNote, Integer> {
 
-
-
-    Page<Notes> findByCreatedBy(Integer userId, Pageable pageable);
-
-    List<Notes> findByCreatedByAndIsDeletedTrue(Integer userId);
-
-    Page<Notes> findByCreatedByAndIsDeletedFalse(Integer userId, Pageable pageable);
-
-    List<Notes> findByIsDeletedAndDeletedAtBefore(boolean b, LocalDateTime cutoffDate);
+    List<FavouriteNote> findByUserId(Integer userId);
 }
