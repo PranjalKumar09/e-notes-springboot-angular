39e169c8b381973c4102587adb473d8c17fadfa3
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-12 19:36:45 +0530

    Add Role.java for user roles management

--- E_Notes/src/main/java/com/pranjal/enitity/Role.java
+++ E_Notes/src/main/java/com/pranjal/enitity/Role.java
@@ -1,4 +1,21 @@
 package com.pranjal.enitity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Role {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Integer id;
+    private String name;
 }
