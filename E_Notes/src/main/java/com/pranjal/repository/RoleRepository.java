1658621e059a7a72b233cb4d4166a88b83af9f5a
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-14 19:36:45 +0530

    Add RoleRepository.java for interacting with Role entities in DB

--- E_Notes/src/main/java/com/pranjal/repository/RoleRepository.java
+++ E_Notes/src/main/java/com/pranjal/repository/RoleRepository.java
@@ -1,10 +1,11 @@
 package com.pranjal.repository;
 
+import com.pranjal.enitity.Role;
 import com.pranjal.enitity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<User, Integer> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 }
