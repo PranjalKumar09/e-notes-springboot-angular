4e69a2fbe4de5fa90448c873af4c1833f344f24d
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-15 19:36:45 +0530

    Add UserRepository.java for managing user entities in DB

--- E_Notes/src/main/java/com/pranjal/repository/UserRepository.java
+++ E_Notes/src/main/java/com/pranjal/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.pranjal.repository;
 
-public interface UserRepository {
+import com.pranjal.enitity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
