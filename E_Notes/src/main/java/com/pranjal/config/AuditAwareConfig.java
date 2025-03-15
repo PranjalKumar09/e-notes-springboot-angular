b62c0b9a9b68c2d0bdd7cce28b352e3e119ea8b8
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-15 18:17:46 +0530

    done the chagnes in logginig

--- E_Notes/src/main/java/com/pranjal/config/AuditAwareConfig.java
+++ E_Notes/src/main/java/com/pranjal/config/AuditAwareConfig.java
@@ -2,16 +2,25 @@ package com.pranjal.config;
 
 import com.pranjal.enitity.User;
 import com.pranjal.util.CommonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.AuditorAware;
 
 import java.util.Optional;
 
+
+@Slf4j
 public class AuditAwareConfig implements AuditorAware<Integer> {
 
     @Override
     public Optional<Integer> getCurrentAuditor() {
-
+        log.info("AuditAwareConfig : getCurrentAuditor()");
         User loggedInUser = CommonUtil.getLoggedInUser();
-        return Optional.of(loggedInUser.getId());
+        if (loggedInUser != null) {
+            log.debug("Current auditor: {}", loggedInUser.getId());
+            return Optional.of(loggedInUser.getId());
+        } else {
+            log.debug("No logged-in user, using default auditor: 0");
+            return Optional.of(0);
+        }
     }
 }
--- E_Notes/src/main/java/com/pranjal/config/security/SecurityConfig.java
+++ E_Notes/src/main/java/com/pranjal/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pranjal.config.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,10 +22,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-    public class SecurityConfig {
+public class SecurityConfig  {
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -34,10 +37,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-
-
 
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
@@ -55,14 +54,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(AbstractHttpConfigurer::disable)
+        log.info("SecurityConfig : securityFilterChain() : Execution Start");
+        http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/**", "/api/v1/home/**")
                         .permitAll().anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-        ;
+
+
+        log.info("SecurityConfig : securityFilterChain() : Execution Start");
 
         return http.build();
     }
