f05dd16af2c38aa4a233e377c4abccbfa89ca0e6
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-22 18:57:35 +0530

    Define AccountStatus enum to manage user account states

--- E_Notes/src/main/java/com/pranjal/enitity/AccountStatus.java
+++ E_Notes/src/main/java/com/pranjal/enitity/AccountStatus.java
@@ -1,4 +1,30 @@
 package com.pranjal.enitity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class AccountStatus {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Integer id;
+    private Boolean isActive;
+    private String verificationCode;
+
+    @OneToOne(mappedBy = "status", optional = false)
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
