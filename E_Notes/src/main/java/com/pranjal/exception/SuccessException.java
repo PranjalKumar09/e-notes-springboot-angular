12de5b33cb929f43e52cbd84c070d3caa6c22fd5
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-23 18:57:35 +0530

    Create SuccessException for handling successful operations with messages

--- E_Notes/src/main/java/com/pranjal/exception/SuccessException.java
+++ E_Notes/src/main/java/com/pranjal/exception/SuccessException.java
@@ -1,3 +1,7 @@
 package com.pranjal.exception;
 
-public class SuccessException exte
+public class SuccessException extends RuntimeException {
+    public SuccessException(String message) {
+        super(message);
+    }
+}
