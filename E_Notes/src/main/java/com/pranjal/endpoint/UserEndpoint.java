54e4717a5ec7861b247a78b866acca883cc8facd
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-24 15:54:02 +0530

    👥 Enhanced UserEndpoint to handle profile updates more efficiently

--- E_Notes/src/main/java/com/pranjal/endpoint/UserEndpoint.java
+++ E_Notes/src/main/java/com/pranjal/endpoint/UserEndpoint.java
@@ -1,17 +1,32 @@
 package com.pranjal.endpoint;
 
 import com.pranjal.dto.PasswordChangeRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Tag(name = "User Management", description = "Operations related to user profile and password management")
 @RequestMapping("/api/v1/user")
 public interface UserEndpoint {
 
+    @Operation(
+            summary = "Get user profile",
+            description = "Retrieves the profile details of the currently authenticated user.",
+            tags = {"User Management"}
+    )
     @GetMapping("/profile")
     ResponseEntity<?> getProfile();
 
-    @GetMapping("/chng-pswd")
+    @Operation(
+            summary = "Change user password",
+            description = "Changes the password of the currently authenticated user.",
+            tags = {"User Management"}
+    )
+    @PostMapping("/chng-pswd")
     ResponseEntity<?> changePassword(@RequestBody PasswordChangeRequest passwordChangeRequest);
 }
