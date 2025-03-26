5de4a7f26757bb24ed67efa5cd17cd0b0d97b41a
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-03-26 18:57:35 +0530

    Implement HomeService with necessary business logic

--- E_Notes/src/main/java/com/pranjal/service/impl/HomeServiceImpl.java
+++ E_Notes/src/main/java/com/pranjal/service/impl/HomeServiceImpl.java
@@ -1,4 +1,37 @@
 package com.pranjal.service.impl;
 
-public class HomeServiceImpl {
+import com.pranjal.enitity.AccountStatus;
+import com.pranjal.enitity.User;
+import com.pranjal.exception.ResourceNotFoundException;
+import com.pranjal.exception.SuccessException;
+import com.pranjal.repository.UserRepository;
+import com.pranjal.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeServiceImpl implements HomeService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public Boolean verifyAccount(Integer userId, String verificationId) throws  Exception{
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Invalid user id"));
+
+
+        if (user.getStatus().getVerificationCode() == null)
+            throw new SuccessException("Account already verified");
+
+        if (user.getStatus().getVerificationCode().equals(verificationId)){
+            AccountStatus status = user.getStatus();
+            status.setIsActive(true);
+            status.setVerificationCode(null);
+
+            user.setStatus(status);
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
 }
