b373e60d8da885cd559240a7c87712347d2cf017
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-03 17:28:09 +0530

    Added TodoRepository.java to handle CRUD operations for TODOs

--- E_Notes/src/main/java/com/pranjal/repository/TodoRepository.java
+++ E_Notes/src/main/java/com/pranjal/repository/TodoRepository.java
@@ -1,4 +1,12 @@
 package com.pranjal.repository;
 
-public interface TodoRepository {
+import com.pranjal.enitity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
+    List<Todo> getTodosByCreatedBy(Integer userId);
 }
