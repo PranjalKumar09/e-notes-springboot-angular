47c453754534d83e254c112ffa751f72f21cfff3
PranjalKumar09 <coderkumarshukla@gmail.com>
2025-04-03 17:28:09 +0530

    Added TodoService.java for service layer handling TODO logic

--- E_Notes/src/main/java/com/pranjal/service/TodoService.java
+++ E_Notes/src/main/java/com/pranjal/service/TodoService.java
@@ -1,4 +1,13 @@
 package com.pranjal.service;
 
+import com.pranjal.dto.TodoDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface TodoService {
+    Boolean saveTodo(TodoDto todoDto) throws Exception;
+    TodoDto getTodoById(Integer id) throws Exception;
+    List<TodoDto> getTodoByUser();
 }
